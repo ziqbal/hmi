c729729fe25002b15ab3998e39d33f145d9e663e
Zafar Iqbal <ultrasine@gmail.com>
2017-09-04 18:12:53 +0300

    test db snippets

--- main.go
+++ main.go
@@ -3,21 +3,50 @@ package main
 
 import (
 
+    "os"
     "log"
+    "fmt"
+    "time"
+    "path/filepath"
     "github.com/boltdb/bolt"
 
 )
 
 func main( ) {
 
-    db, err := bolt.Open( "hmi.db" , 0600 , nil )
+    fmt.Println( "HMI r0.1.YYYYMMDDHHSS (C)2017 Zafar Iqbal <ultrasine@gmail.com> " ) //_REV_
+
+    //////////////////////////////////////////////////////////
+
+    ex, err := os.Executable( )
+
+    if( err != nil ) {
+    
+        panic( err )
+
+    }
+
+    exPath := filepath.Dir( ex )
+
+    //////////////////////////////////////////////////////////
+
+    db, err := bolt.Open( exPath + "/hmi.db" , 0600 , &bolt.Options{ Timeout : 1 * time.Second } )
 
     if( err != nil ) {
 
+        log.Fatal( "Database Opening Error! Exiting..." )
         log.Fatal( err )
 
     }
 
     defer db.Close( )
+
+    //////////////////////////////////////////////////////////
+
+    time.Sleep( 100 * time.Millisecond )
+
+    //////////////////////////////////////////////////////////
+
+    fmt.Println( "FINI" )
 
 }
