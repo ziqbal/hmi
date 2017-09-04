e65caf485ee20328410b72b76013ba9085f6e7dc
Zafar Iqbal <ultrasine@gmail.com>
2017-09-04 17:13:45 +0300

    testing boltdb

---
+++ main.go
@@ -0,0 +1,23 @@
+
+package main
+
+import (
+
+    "log"
+    "github.com/boltdb/bolt"
+
+)
+
+func main( ) {
+
+    db, err := bolt.Open( "hmi.db" , 0600 , nil )
+
+    if( err != nil ) {
+
+        log.Fatal( err )
+
+    }
+
+    defer db.Close()
+
+}
